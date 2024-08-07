6f88e64e596173eae6a51c197f14f7d8405e2252
Yuliya Azovtсeva <yulia.pryania@gmail.com>
2024-08-07 09:23:04 +0400

    task08 v1.0

---
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -0,0 +1,40 @@
+package com.task08;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+
+@LambdaHandler(lambdaName = "api_handler",
+        roleName = "api_handler-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED,
+        layers = {"open-meteo-api-layer"}
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE
+)
+@LambdaLayer(
+        layerName = "open-meteo-api-layer",
+        libraries = {"libs/open-meteo-api-1.0.jar"},
+        runtime = DeploymentRuntime.JAVA11,
+        artifactExtension = ArtifactExtension.ZIP
+)
+public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final int SC_OK = 200;
+
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+        var apiClient = new OpenMeteoApi();
+
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(SC_OK)
+                .withBody(apiClient.getWeatherForecast());
+    }
+}
